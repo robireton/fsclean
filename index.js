c0a0e7fb844b28bef8cfd55b21ac94a0c69a9d62
Rob Ireton <rob@ireton.org>
2022-05-02 10:07:42 -0400

    categorization

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { readdir, rmdir, rm } from 'fs/promises'
-import { extname, join, resolve } from 'path'
+import { mkdir, readdir, rename, rmdir, rm } from 'fs/promises'
+import { dirname, extname, join, resolve } from 'path'
 import { argv } from 'process'
 import { imageSize } from 'image-size'
 
+const categories = {
+  image: ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.psd', '.svg', '.tiff', '.webp'],
+  video: ['.avi', '.mov', '.mpg', '.mp4', '.wmv', '.mpeg']
+}
+
 async function directory (path, options) {
+  if (options.verbose) console.error(path)
   const hidden = []
   const links = []
   const files = []
@@ -13,12 +19,18 @@ async function directory (path, options) {
   }
   if (options.orphans) found.orphans = []
   if (options.sizes) found.sizes = []
+  if (options.categorize) {
+    found.images = []
+    found.videos = []
+    found.others = []
+    found.mixed = []
+  }
   try {
     // process the contents of this directory
     for (const file of await readdir(path, { withFileTypes: true })) {
       if (file.name.startsWith('.')) {
-        if (file.name === '.DS_Store' && options.delete) {
-          await rm(join(path, file.name))
+        if (file.name === '.DS_Store') {
+          if (options.delete) await rm(join(path, file.name))
         } else {
           hidden.push(join(path, file.name))
         }
@@ -45,22 +57,34 @@ async function directory (path, options) {
       } else if (options.orphans && directories.length === 0 && files.length === 1) {
         // orphan file
         found.orphans.push(files[0])
-      } else if (options.sizes && directories.length === 0 && files.length > 0) {
-        const images = ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.psd', '.svg', '.tiff', '.webp']
+      } else if ((options.sizes || options.categorize) && directories.length === 0 && files.length > 0) {
         const sizes = []
+        const extensions = []
         for (const f of files) {
-          if (images.includes(extname(f).toLowerCase())) {
-            try {
-              const dimensions = imageSize(f)
-              if (options.verbose) console.log(f, dimensions)
-              sizes.push(dimensions.height * dimensions.width)
-            } catch (sizeError) {
-              console.error(`${f} ${sizeError.message}`)
+          const extension = extname(f).toLowerCase()
+          if (options.sizes) {
+            if (categories.image.includes(extension)) {
+              try {
+                const dimensions = imageSize(f)
+                // if (options.verbose) console.error(f, dimensions)
+                sizes.push(dimensions.height * dimensions.width)
+              } catch (sizeError) {
+                console.error(`${f} ${sizeError.message}`)
+              }
             }
+          }
+          if (options.categorize) {
+            extensions.push(extension)
           }
         }
         if (sizes.length > 0) {
           found.sizes.push({ path: path, size: Math.floor(sizes.reduce((previous, current) => (previous + current)) / sizes.length) })
+        }
+        if (options.categorize) {
+          if (extensions.every(e => categories.image.includes(e))) found.images.push(path)
+          else if (extensions.every(e => categories.video.includes(e))) found.videos.push(path)
+          else if (!(extensions.some(e => categories.image.includes(e)) || extensions.some(e => categories.video.includes(e)))) found.others.push(path)
+          else found.mixed.push(path)
         }
       }
     }
@@ -74,6 +98,7 @@ async function directory (path, options) {
 async function find () {
   const [, , ...args] = argv
   const options = {
+    categorize: false,
     delete: false,
     orphans: false,
     verbose: false,
@@ -81,25 +106,32 @@ async function find () {
   }
   const dirs = new Set()
   for (const arg of args) {
-    if (arg === '-d') {
-      options.delete = true
-      continue
+    switch (arg) {
+      case '-c':
+        options.categorize = true
+        break
+
+      case '-d':
+        options.delete = true
+        break
+
+      case '-o':
+        options.orphans = true
+        break
+
+      case '-s':
+        options.sizes = true
+        break
+
+      case '-v':
+        options.verbose = true
+        break
+
+      default:
+        dirs.add(resolve(arg))
     }
-    if (arg === '-o') {
-      options.orphans = true
-      continue
-    }
-    if (arg === '-s') {
-      options.sizes = true
-      continue
-    }
-    if (arg === '-v') {
-      options.verbose = true
-      continue
-    }
-    dirs.add(resolve(arg))
   }
-  if (options.verbose) console.log('options: %O', options)
+  if (options.verbose) console.error('options: %O', options)
   for (const d of dirs) {
     console.log(`\nStarting at ${d}\n`)
     const results = await directory(d, options)
@@ -127,8 +159,73 @@ async function find () {
 
     if (options.sizes) {
       for (const p of results.sizes.sort((a, b) => (a.size - b.size))) {
-        console.log(`${Math.floor(p.size / 100000) / 10}\t${p.path}`)
+        console.log(`${(p.size / 1000000).toFixed(1)}\t${p.path}`)
       }
+    }
+
+    if (options.categorize) {
+      if (results.images.length > 0) {
+        if (options.verbose) console.log('\nImage Directories:')
+        const imageDir = join(d, '_images')
+        for (const p of results.images) {
+          if (p.startsWith(imageDir)) continue
+          const destination = p.replace(d, imageDir)
+          if (options.verbose) console.log(`${p} -> ${destination}`)
+          try {
+            await mkdir(dirname(destination), { recursive: true })
+            await rename(p, destination)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      } else {
+        if (options.verbose) console.log('\nNo Image Directories')
+      }
+
+      if (results.videos.length > 0) {
+        if (options.verbose) console.log('\nVideo Directories:')
+        const videoDir = join(d, '_videos')
+        for (const p of results.videos) {
+          if (p.startsWith(videoDir)) continue
+          const destination = p.replace(d, videoDir)
+          if (options.verbose) console.log(`${p} -> ${destination}`)
+          try {
+            await mkdir(dirname(destination), { recursive: true })
+            await rename(p, destination)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      } else {
+        if (options.verbose) console.log('\nNo Video Directories')
+      }
+
+      if (results.mixed.length > 0) {
+        if (options.verbose) console.log('\nMixed Directories:')
+        const mixedDir = join(d, '_mixed')
+        for (const p of results.mixed) {
+          if (p.startsWith(mixedDir)) continue
+          const destination = p.replace(d, mixedDir)
+          if (options.verbose) console.log(`${p} -> ${destination}`)
+          try {
+            await mkdir(dirname(destination), { recursive: true })
+            await rename(p, destination)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      } else {
+        if (options.verbose) console.log('\nNo Mixed Directories')
+      }
+
+      // if (results.others.length > 0) {
+      //   console.log('\nOther Directories:')
+      //   for (const p of results.others) {
+      //     console.log(p)
+      //   }
+      // } else {
+      //   console.log('\nNo Other Directories')
+      // }
     }
   }
 }
